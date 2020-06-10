b9908a00c0affc9621864e91b2b0d27d272b3671
Kyle Melton <kalm42@gmail.com>
2020-06-10 08:30:01 -0600

    feat: add cors support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import express from "express"
 import bodyParser from "body-parser"
 import { ApolloServer } from "apollo-server-express"
+import Stripe from "stripe"
 import { environment } from "./environment"
 import resolvers from "./resolvers"
 import typeDefs from "./type-defs"
+import cors from "cors"
+
+const FRONTEND = process.env.FRONTEND
+const key = process.env.STRIPE_KEY
+const stripe = new Stripe(key, {
+  apiVersion: "2020-03-02",
+})
 
 const server = new ApolloServer({
   resolvers,
@@ -14,9 +22,21 @@ const server = new ApolloServer({
 
 const app = express()
 
+app.use(cors({ origin: FRONTEND }))
+
 app.use(express.json())
 
 server.applyMiddleware({ app })
+
+app.get("/secret", async (req, res) => {
+  const intent = await stripe.paymentIntents.create({
+    amount: 500,
+    currency: "usd",
+    metadata: { integration_check: "accept_a_payment" },
+  })
+
+  res.json({ client_secret: intent.client_secret })
+})
 
 app.listen({ port: environment.port }, () => {
   console.log(`Server ready at http://localhost:${environment.port}${server.graphqlPath}`)
